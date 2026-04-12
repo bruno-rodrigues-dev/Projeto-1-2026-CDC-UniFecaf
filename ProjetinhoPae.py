56b4e3ec87e44db68c78f12ad78d9d19a4110052
Pedro Henrique <133719843+Pedroo-M@users.noreply.github.com>
2026-04-12 18:35:02 -0300

    Criação da classe Anuncios, junto com id do usuario. Login feito usando o user como parâmetro

--- ProjetinhoPae.py
+++ ProjetinhoPae.py
@@ -1,29 +1,56 @@
 class Usuario():
-    def __init__(self, email, senha):
+    def __init__(self,id , email, senha, nome):
+        self.id = id
         self.email = email
         self.senha = senha
-#aqui é a classe usuario e tals
+        self.nome = nome
+
+class Anuncios():
+    def __init__(self,id , vendedor, nome, preco ):
+        self.id = id
+        self.vendedor = vendedor
+        self.nome = nome
+        self.preco = preco
+
 class Repo():
     def __init__(self):
         self.usuarios = []
-#esse vai ser o "banco de dados por enquanto", aonde vamos guardar os usuarios
+        self.anuncios = []
+        self.next_user_id = 1
+        self.next_anuncio_id = 1
+
     def mostrarUsuarios(self):
-        n = 1
         for i in self.usuarios:
-            print(n, f' - Email: {i.email} | Senha: {i.senha}')
-            n += 1
+            print(f'{i.id} - Email: {i.email} | Senha: {i.senha}')
+
+    def mostrarAnuncios(self, user):
+        for i in self.anuncios:
+            if i.vendedor == user.id :
+                print(f'ID do Anuncio: {i.id} - {i.nome} - Preço: {i.preco} | Id Vendedor: {i.vendedor}')
 
     def addUsuarioRepo(self, usuario):
+        self.next_user_id += 1
         self.usuarios.append(usuario)
 
-    def validarEmail(self, email):
+    def addAnuncioRepo(self, anuncio):
+        self.next_anuncio_id += 1
+        self.anuncios.append(anuncio)
+
+    def emailExiste(self, email):
         for n in self.usuarios:
             if n.email == email:
-                print('Email já existente!!!')
                 return False
         return True
+
+    def validadorSenha(self, email, senha):
+        for n in self.usuarios:
+            if n.email == email:
+                if n.senha == senha:
+                    return n
+        return False
+
 repo = Repo() #aqui eu criei um objeto com base na classe Repo (repo de repositorio)
-def menu():
+def menuLogin():
     print('1 - Cadastro')
     print('2 - Login')
     print('0 - Sair')
@@ -34,22 +61,70 @@ def menu():
         funcaoCadastro()
 
     elif opcao == 2:
-        # funcaoLogin, ainda nao fiz
+        user = funcaoLogin()
+        if user:
+            while True:
+                if menuPrincipal(user) == False:
+                    break
         pass
 
     elif opcao == 0:
         return False
 
+def menuPrincipal(user):
+    print('1 - Criar Anuncio')
+    print('2 - Meus Anuncios')
+    print('3 - Editar Anuncios')
+    print('4 - Remover Anuncios')
+    print('0 - Sair da Conta')
+
+    opcao = int(input('Escolha: '))
+
+    if opcao == 1:
+        criarAnuncio(user)
+
+    elif opcao == 2:
+        repo.mostrarAnuncios(user)
+
+    elif opcao == 3:
+        pass
+
+    elif opcao == 4:
+        pass
+
+    elif opcao == 0:
+        return False
+
+
 def funcaoCadastro():
     email = input('Qual o email para cadastro?')
-    if repo.validarEmail(email):
+    if repo.emailExiste(email):
         senha = input('Digite uma senha para o cadastro: ')
-        usuario = Usuario(email, senha)
+        nome = input('Digite o seu nome: ')
+        usuario = Usuario(repo.next_user_id,email, senha, nome)
         repo.addUsuarioRepo(usuario)
         repo.mostrarUsuarios()
     else:
+        print('Email já existente!!!')
         return
 
+def funcaoLogin ():
+    email = input('Qual o seu email de login?')
+    if not repo.emailExiste(email):
+        senha = input('Digite a sua senha:')
+        user = repo.validadorSenha(email, senha)
+        print('Seja bem vindo: ', user.nome)
+        return user
+    else:
+        print('Email Inválido!!!')
+        return
+
+def criarAnuncio(user):
+    nome = input('Qual o nome do anuncio?')
+    preco = float(input('Qual o preço do anuncio?'))
+    anuncio = Anuncios(repo.next_anuncio_id, user.id, nome, preco)
+    repo.addAnuncioRepo(anuncio)
+
 while True:
-    if menu() == False:
+    if menuLogin() == False:
         break
