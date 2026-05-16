18ad606873926c8c70318db17c1cae923c06da72
Pedro Henrique <133719843+Pedroo-M@users.noreply.github.com>
2026-05-16 16:42:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,1 @@
-import mysql.connector # Use assim
 
-def ligar_banco():
-    try:
-        connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='Ackerman@20',
-            database='projeto_unifecaf'
-        )
-        return connection
-    except mysql.connector.Error as e:
-        print(f"Erro ao conectar: {e}")
-        return None
